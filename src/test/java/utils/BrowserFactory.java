c24e1f9d203c86cf7a858c5458d9186849bec2e6
prtraj1 <prtraj009@gmail.com>
2022-09-27 22:14:10 +0530

    Updated to execute in docker

--- src/test/java/utils/BrowserFactory.java
+++ src/test/java/utils/BrowserFactory.java
@@ -2,6 +2,11 @@ package utils;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class BrowserFactory {
 
@@ -14,6 +19,14 @@ public class BrowserFactory {
     public synchronized static void setDriver(String browserName) {
         if (browserName.equalsIgnoreCase("edge")) {
             driver.set(WebDriverManager.edgedriver().create());
+        } else if (browserName.equalsIgnoreCase("docker")) {
+            ChromeOptions chromeOptions = new ChromeOptions();
+            chromeOptions.addArguments("--disable-dev-shm-usage");
+            try {
+                driver.set(new RemoteWebDriver(new URL("http://localhost:4444/"), chromeOptions));
+            } catch (MalformedURLException e) {
+                throw new RuntimeException(e);
+            }
         } else {
             driver.set(WebDriverManager.chromedriver().create());
         }
